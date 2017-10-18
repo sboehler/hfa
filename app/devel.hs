9e625e174da57926916bfc64e7d7241b85a85248
sboehler <sboehler@noreply.users.github.com>
2017-10-18 22:37:24 +0200

    Initial commit

---
+++ app/devel.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE PackageImports #-}
+
+import "prototype" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
---
+++ src/Foundation.hs
@@ -0,0 +1,111 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module Foundation where
+
+import Import.NoFoundation
+import Text.Jasmine (minifym)
+import Yesod.Core.Types (Logger)
+import qualified Yesod.Core.Unsafe as Unsafe
+import Yesod.Default.Util (addStaticContentExternal)
+
+-- | The foundation datatype for your application. This can be a good place to
+-- keep settings and values requiring initialization before your application
+-- starts running, such as database connections. Every handler will have
+-- access to the data present here.
+data App = App
+  { appSettings :: AppSettings
+  , appStatic :: Static -- ^ Settings for static file serving.
+  , appHttpManager :: Manager
+  , appLogger :: Logger
+  }
+
+-- This is where we define all of the routes in our application. For a full
+-- explanation of the syntax, please see:
+-- http://www.yesodweb.com/book/routing-and-handlers
+--
+-- Note that this is really half the story; in Application.hs, mkYesodDispatch
+-- generates the rest of the code. Please see the following documentation
+-- for an explanation for this split:
+-- http://www.yesodweb.com/book/scaffolding-and-the-site-template#scaffolding-and-the-site-template_foundation_and_application_modules
+--
+-- This function also generates the following type synonyms:
+-- type Handler = HandlerT App IO
+-- type Widget = WidgetT App IO ()
+mkYesodData "App" $(parseRoutesFile "config/routes")
+
+-- Please see the documentation for the Yesod typeclass. There are a number
+-- of settings which can be configured by overriding methods here.
+instance Yesod App
+    -- Controls the base of generated URLs. For more information on modifying,
+    -- see: https://github.com/yesodweb/yesod/wiki/Overriding-approot
+                                                                      where
+  approot =
+    ApprootRequest $ \app req ->
+      case appRoot $ appSettings app of
+        Nothing -> getApprootText guessApproot app req
+        Just root -> root
+    -- Store session data on the client in encrypted cookies,
+    -- default session idle timeout is 120 minutes
+  makeSessionBackend _ =
+    Just <$>
+    defaultClientSessionBackend
+      120 -- timeout in minutes
+      "config/client_session_key.aes"
+    -- Yesod Middleware allows you to run code before and after each handler function.
+    -- The defaultYesodMiddleware adds the response header "Vary: Accept, Accept-Language" and performs authorization checks.
+    -- Some users may also want to add the defaultCsrfMiddleware, which:
+    --   a) Sets a cookie with a CSRF token in it.
+    --   b) Validates that incoming write requests include that token in either a header or POST parameter.
+    -- To add it, chain it together with the defaultMiddleware: yesodMiddleware = defaultYesodMiddleware . defaultCsrfMiddleware
+    -- For details, see the CSRF documentation in the Yesod.Core.Handler module of the yesod-core package.
+  yesodMiddleware = defaultYesodMiddleware
+    -- Routes not requiring authenitcation.
+  isAuthorized FaviconR _ = return Authorized
+  isAuthorized RobotsR _ = return Authorized
+    -- Default to Authorized for now.
+  isAuthorized _ _ = return Authorized
+    -- This function creates static content files in the static folder
+    -- and names them based on a hash of their content. This allows
+    -- expiration dates to be set far in the future without worry of
+    -- users receiving stale content.
+  addStaticContent ext mime content = do
+    master <- getYesod
+    let staticDir = appStaticDir $ appSettings master
+    addStaticContentExternal
+      minifym
+      genFileName
+      staticDir
+      (StaticR . flip StaticRoute [])
+      ext
+      mime
+      content
+        -- Generate a unique filename based on the content itself
+    where
+      genFileName lbs = "autogen-" ++ base64md5 lbs
+    -- What messages should be logged. The following includes all messages when
+    -- in development, and warnings and errors in production.
+  shouldLog app _source level =
+    appShouldLogAll (appSettings app) ||
+    level == LevelWarn || level == LevelError
+  makeLogger = return . appLogger
+
+-- Useful when writing code that is re-usable outside of the Handler context.
+-- An example is background jobs that send email.
+-- This can also be useful for writing code that works across multiple Yesod applications.
+instance HasHttpManager App where
+  getHttpManager = appHttpManager
+
+unsafeHandler :: App -> Handler a -> IO a
+unsafeHandler = Unsafe.fakeHandlerGetLogger appLogger
+-- Note: Some functionality previously present in the scaffolding has been
+-- moved to documentation in the Wiki. Following are some hopefully helpful
+-- links:
+--
+-- https://github.com/yesodweb/yesod/wiki/Sending-email
+-- https://github.com/yesodweb/yesod/wiki/Serve-static-files-from-a-separate-domain
+-- https://github.com/yesodweb/yesod/wiki/i18n-messages-in-the-scaffolding
